c47b4364309b24e25233a3b9ad58d3d61e75ac75
charankkkumar <86058806+charankkkumar@users.noreply.github.com>
2022-04-12 18:12:51 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+function preload()
+{
+    img = loadImage('mario.jpg');
+}
+function setup()
+
+{
+    canvas = createCanvas(380, 380);
+    canvas.center();
+    video = createCapture(VIDEO);
+    video.size(380, 380);
+    video.hide();
+    objectDetector = ml5.objectDetector('cocossd',modelLoaded);
+    document.getElementById("status").innerHTML = "Status : Detecting Objects";
+}
